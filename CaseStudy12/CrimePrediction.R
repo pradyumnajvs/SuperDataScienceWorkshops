a5b1ae324128bb979327d2b552f7afef065aeccf
pradyumnajvs <jvs.pradyumna@gmail.com>
2017-06-01 16:04:10 -0500

    Update CrimePrediction.R

--- CaseStudy12/CrimePrediction.R
+++ CaseStudy12/CrimePrediction.R
@@ -32,28 +32,49 @@ model_data <- data[, names(data) %in% c('ViolentCrimesPerPop', 'pctUrban',
                                         'agePct16t24', 'PctUnemployed', 'medIncome')]
 
 # 3. Check for correlations between medIncome and PctUnemployed. Plot these variables to confirm correlations
-
 cor(model_data$medIncome, model_data$PctUnemployed)
 cor(model_data$medIncome, model_data$ViolentCrimesPerPop)
 cor(model_data$PctUnemployed, model_data$ViolentCrimesPerPop)
 cor(model_data$PctUnemployed, model_data$agePct16t24)
 
+plot(model_data$medIncome, model_data$PctUnemployed,
+xlab = "Median Household Income",
+ylab = " percentage of people 16 and over, in the labor force, and unemployed") 
++ title(main ="Median Income per percentage of people 16 and over, in the labor force, and unemployed")
 
+plot(model_data$medIncome, model_data$ViolentCrimesPerPop, 
+     xlab = "Median Household Income",
+     ylab = " Total amount of crimes per 100K population") 
++ title(main ="Median Income per Total amount of crimes")
+
+plot(model_data$PctUnemployed, model_data$ViolentCrimesPerPop, 
+     xlab = "Percentage of Unemployed Population",
+     ylab = "Total amount of crimes per population")
++ title(main ="Percentage of Unemployed Population per Total amount of crimes per 100K population")
+
+plot(model_data$PctUnemployed, model_data$agePct16t24, 
+     xlab = "Percentage of Unemployed Population",
+     ylab = "percentage of population that is 16-24 in age")
++ title(main ="Percentage of Unemployed Population per percentage of population that is 16-24 in age")
+
+#4. Split the data into training and testing sets using set.seed(123)
 set.seed(123)
 split = sample.split(model_data$ViolentCrimesPerPop, SplitRatio = 0.8)
 
 data_train <- model_data[split,] 
 data_test <- model_data[!split,]
 
+#5. Build a linear regression model using pctUrban, agePct16t24 and whichever of the variables from 
+# question 3 is best correlated with ViolentCrimesPerPop
 model = lm(formula = ViolentCrimesPerPop ~ medIncome + PctUnemployed + pctUrban + agePct16t24, data = data_train)
 model = lm(formula = ViolentCrimesPerPop ~ medIncome + PctUnemployed + pctUrban, data = data_train)
 summary(model)
 
+#6. Predict the ViolentCrimesPerPop for the testing set
 test_predicted_data <- predict(model, newdata = data_test)
 
+#7. Calculate the R2 of the testing set
 ss_residual <- sum((test_predicted_data - data_test$ViolentCrimesPerPop)^2)
 ss_total <- sum((data_test$ViolentCrimesPerPop - mean(data_test$ViolentCrimesPerPop))^2)
-
 r_square <- 1 - ss_residual/ss_total
-
 r_square
