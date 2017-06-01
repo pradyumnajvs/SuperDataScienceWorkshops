b97968fcb8eb3b73a6ef10f187a1dd1371631cb3
pradyumnajvs <jvs.pradyumna@gmail.com>
2017-06-01 15:39:37 -0500

    Update CrimePrediction.R

--- CaseStudy12/CrimePrediction.R
+++ CaseStudy12/CrimePrediction.R
@@ -15,20 +15,29 @@ data <- read.csv("~/DataScience/Case Study/Case Study 12/data.csv", header = FAL
 str(var_name)
 str(data)
 
+#1. Clean variable_names and set the names of data to these clean names
+
+#Let us clean the first column by removing the first 4 letters in the entire phrase.
 var_name$clean = substr(var_name$V1, 4, 1000)
+#We will only consider the word before ':' in the entire string
 var_name$clean = gsub(":.+",'', var_name$clean)
-
+#Now assign the words in Clean column from var_name dataset as column names for dataset named data
 names(data) <- var_name$clean
-
+#Check to make sure that the column names are properly assigned
 names(data)
 
+#2. The police chief is only interested in the following variables so create a new data frame with
+#just these variables included: 'ViolentCrimesPerPop', 'pctUrban', 'agePct16t24', 'PctUnemployed', 'medIncome'
 model_data <- data[, names(data) %in% c('ViolentCrimesPerPop', 'pctUrban',
                                         'agePct16t24', 'PctUnemployed', 'medIncome')]
+
+# 3. Check for correlations between medIncome and PctUnemployed. Plot these variables to confirm correlations
 
 cor(model_data$medIncome, model_data$PctUnemployed)
 cor(model_data$medIncome, model_data$ViolentCrimesPerPop)
 cor(model_data$PctUnemployed, model_data$ViolentCrimesPerPop)
 cor(model_data$PctUnemployed, model_data$agePct16t24)
+
 
 set.seed(123)
 split = sample.split(model_data$ViolentCrimesPerPop, SplitRatio = 0.8)
